58b7e0dd6d3dae7822157eeecce8af422639c06c
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-02 20:09:11 -0300

    criando método get e put by id)

--- src/app.js
+++ src/app.js
@@ -12,13 +12,17 @@ const livros = [
     {id: 2, "titulo": "O Hobbit"}
 ]
 
-// criando rotas
+// criando método GETT
 app.get('/', (req, res) => {
     res.status(200).send('Curso de Node');
 })
-
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.param.id);
+    res.json(livros[index]);
 })
 
 // criando método POST 
@@ -26,5 +30,17 @@ app.post('/livros', (req, res) => {
     livros.push(req.body);
     res.status(201).send('Livro foi cadastrado com sucesso')
 })
+
+// criando método PUT
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.param.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+// criando função para buscar livro por id
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
