6f2e9b155ca02b2e3d86c50ad02844fa36a77259
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-02 12:30:09 -0300

    refatorando para importar app + boas praticas port

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
-// criando servidor com require (CommonJS)
+import app from './src/app.js'
 
-// atribuindo variaveis
-const http = require("http")
-const port = 3000;
+// criando porta 
+const port = process.end.PORT || 3000;
 
-// criando objt de rota
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na pag de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pag de editora',
-    '/sobre': 'Info sobre o projeto'
-}
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-})
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
