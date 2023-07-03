09aa13b5d9508217d1986287dedbba0bb3be4638
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-03 15:01:37 -0300

    metodo put + controller atualizarLivro

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -8,5 +8,6 @@ const router = express.Router();
 router
   .get("/livros", LivroController.listarLivros)
   .post("/livros", LivroController.cadastrarLivro)
+  .put("/livros/:id", LivroController.atualizarLivro)
 
 export default router;
