1129caf36d49141ee0fc35368ea45a8d07b1ed8c
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-03 14:17:32 -0300

    metodo post para cadastrarlivro

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import LivroController from "../controlllers/livrosController.js";
+import LivroController from "../controllers/livrosController.js";
 
 // atribui variavel
 const router = express.Router();
 
 // cria rota de requisição
 router
-  .get("./livros", LivroController.listarLivros())
+  .get("/livros", LivroController.listarLivros)
+  .post("/livros", LivroController.cadastrarLivro)
 
 export default router;
