4320ce68d5226e753749957ca571b21b534cc1d3
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-03 16:13:33 -0300

    metodo delete + controller excluirLivro

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -10,5 +10,6 @@ router
   .get("/livros/:id", LivroController.listarLivrosPorId)
   .post("/livros", LivroController.cadastrarLivro)
   .put("/livros/:id", LivroController.atualizarLivro)
+  .delete("/livros/:id", LivroController.excluirLivro)
 
 export default router;
