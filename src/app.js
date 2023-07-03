2f81de34e3b75c2a19c452be5ceb253a2f01a57c
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-03 16:19:55 -0300

    cleaning app

--- src/app.js
+++ src/app.js
@@ -19,37 +19,4 @@ app.use(express.json());
 // instanciando rotas
 routes(app);
 
-// criando um array de livros
-// const livros = [
-//     {id: 1, "titulo": "O Senhor do Anéis"},
-//     {id: 2, "titulo": "O Hobbit"}
-// ]
-
-// criando método GET
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.param.id);
-    res.json(livros[index]);
-})
-
-// criando método PUT
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.param.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-})
-
-// criando método DELETE
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    // metodo splice (delete) a partir do index, apenas 1 elemento
-    livros.splice(index, 1)
-    res.send(`Livro ${id} removido com sucesso`);
-})
-
-// criando função para buscar livro por id
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
-
 export default app
