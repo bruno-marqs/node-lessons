79fbc2ee780ff6bd61e023510fdaf4b9d7880048
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-07 20:16:09 -0300

    import passport + rota login + autenticacao

--- blog-do-codigo/src/usuarios/usuarios-rotas.js
+++ blog-do-codigo/src/usuarios/usuarios-rotas.js
@@ -1,6 +1,12 @@
 const usuariosControlador = require('./usuarios-controlador');
+const passport = require('passport');
 
 module.exports = app => {
+  // criando a rota de login
+  app
+    .route('/usuario/login')
+    .post(passport.authenticate('local', { session : false }), usuariosControlador.login)
+
   app
     .route('/usuario')
     .post(usuariosControlador.adiciona)
