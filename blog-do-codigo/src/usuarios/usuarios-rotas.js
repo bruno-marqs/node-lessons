de11cb6d5ef49212a838d6c176dd1418711ec2b6
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-07 22:02:58 -0300

    refatorando + import tratamento erro token

--- blog-do-codigo/src/usuarios/usuarios-rotas.js
+++ blog-do-codigo/src/usuarios/usuarios-rotas.js
@@ -1,6 +1,5 @@
 const usuariosControlador = require('./usuarios-controlador');
 const middlewaresAutenticacao = require('./middlewares-autenticacao')
-const passport = require('passport');
 
 module.exports = app => {
   // criando a rota de login
@@ -19,7 +18,7 @@ module.exports = app => {
   app
     .route('/usuario/:id')
     .delete(
-      passport.authenticate('bearer', { session: false }), 
+      middlewaresAutenticacao.bearer, 
       usuariosControlador.deleta
     );
 };
