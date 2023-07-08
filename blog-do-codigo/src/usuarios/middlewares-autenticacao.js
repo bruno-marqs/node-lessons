aba853d47a082f29fa9ec7ef8ef51e0a14a070e1
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-07-07 22:02:06 -0300

    tratamento de exceções no portador token

--- blog-do-codigo/src/usuarios/middlewares-autenticacao.js
+++ blog-do-codigo/src/usuarios/middlewares-autenticacao.js
@@ -22,6 +22,29 @@ module.exports = {
                 return next();
             }
         )(req, res, next);
+    },
+
+    bearer: (req, res, next) => {
+        passport.authenticate(
+            'bearer',
+            {session: false},
+            (erro, usuario, info) => {
+                if(erro && erro.nome === 'JsonWebTokenError') {
+                    return res.status(401).json({ erro: erro.message });
+                }
+                
+                if (erro) {
+                    return res.status(500).json({ erro: erro.message })
+                }
+
+                if(!usuario) {
+                    return res.status(401).json();
+                }
+
+                req.user = usuario;
+                return next();
+            }
+        )(req, res, next)
     }
         
 };
